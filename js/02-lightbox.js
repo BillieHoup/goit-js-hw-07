38e4ce5dc90f7e6d1baa9ab2562b02309a3ccd43
BillieHoup <95242599+BillieHoup@users.noreply.github.com>
2023-11-12 03:05:40 +0200

    d

---
+++ js/02-lightbox.js
@@ -0,0 +1,20 @@
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function galleryMarkup(items) {
+    return items.map((item) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+           <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>`
+    ).join("");
+};
+
+const addGalleryMarkup = galleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", addGalleryMarkup);
+
+    var lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionsDelayTime: 250});
